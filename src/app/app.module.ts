aed9e24bcbd8b350d3d4eaea004e65bbb26f685e
Kouassi15 <rachelleservices8@gmail.com>
2023-01-13 21:16:55 +0100

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { ProductComponent } from './product/product.component';
+import { TestPipe } from './test.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    GreetingComponent,
+    ProductComponent,
+    TestPipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
